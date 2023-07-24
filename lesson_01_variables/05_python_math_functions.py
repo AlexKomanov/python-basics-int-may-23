08168db29611fb78a25d27934984a9b18bc31f34
AlexKomanov <akomanov88@gmail.com>
2023-07-24 18:57:23 +0300

    adding 05_python_math_functions.py

---
+++ lesson_01_variables/05_python_math_functions.py
@@ -0,0 +1,32 @@
+# abs()
+number = abs(-56.7)
+print(number)
+
+print(abs(-120))
+
+# min()
+
+number = min(23, 5, 60, 120, -200)
+print(number)
+
+# max()
+max_number = max(23, 5, 60, 120, -200)
+print(max_number)
+
+
+print(pow(5, 3))  # --> print(5 ** 3)
+
+# round()
+
+print(round(3.456456456456456))
+print(round(3.656456456456456))
+print(round(3.456453456456456, 5))
+print(round(3.99445, 2))
+print(round(3.99645, 2))
+print(round(3.99545, 2))
+
+print(pow(25, 0.5))
+print(25 ** 0.5)
+
+
+
