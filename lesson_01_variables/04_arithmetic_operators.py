40a5355bfbcbe2b4a7787212d3ebb7bca085b7ed
AlexKomanov <akomanov88@gmail.com>
2023-07-24 18:24:14 +0300

    adding 04_arithmetic_operators.py

---
+++ lesson_01_variables/04_arithmetic_operators.py
@@ -0,0 +1,59 @@
+# number_1 = 9
+# number_2 = 4
+#
+# text_1 = "alex"
+# text_2 = "komanov"
+#
+# print(f"{text_1} + {text_2} = {(text_1 + text_2)}")
+#
+# print(f"{number_1} + {number_2} = {(number_1 + number_2)}")
+# print(number_1, " + ", number_2, " = ", (number_1 + number_2))
+#
+# print(f"{number_1} - {number_2} = {(number_1 - number_2)}")
+#
+# text_1 = "**"
+# print(f"{text_1} * {number_2} = {(text_1 * number_2)}")
+#
+# print(f"{number_1} * {number_2} = {(number_1 * number_2)}")
+# print(type(number_1 * number_2))
+#
+# number_3 = 2.0
+#
+# print(f"{number_1} * {number_3} = {(number_1 * number_3)}")
+# print(type(number_1 * number_3))
+#
+# print(f"{number_1} / {number_2} = {(number_1 / number_2)}")
+#
+# number_1 = 2
+# number_2 = 5
+# print(f"{number_1} ** {number_2} = {(number_1 ** number_2)}")
+
+# ----------------------------------------
+
+number_1 = 15
+number_2 = 5
+print(f"{number_1} // {number_2} = {(number_1 // number_2)}")
+
+number_1 = 16
+number_2 = 5
+print(f"{number_1} / {number_2} = {(number_1 / number_2)}")
+print(f"{number_1} // {number_2} = {(number_1 // number_2)}")    # 16 : 5 = 3   -> 3 * 5 => 15 + (1) -> 16
+print(f"{number_1} % {number_2} = {(number_1 % number_2)}")
+
+number_1 = 20
+number_2 = 5
+print(f"{number_1} / {number_2} = {(number_1 / number_2)}")
+print(f"{number_1} // {number_2} = {(number_1 // number_2)}")
+print(f"{number_1} % {number_2} = {(number_1 % number_2)}")
+
+number_1 = 5
+number_2 = 15
+print(f"{number_1} / {number_2} = {(number_1 / number_2)}")
+print(f"{number_1} // {number_2} = {(number_1 // number_2)}")
+print(f"{number_1} % {number_2} = {(number_1 % number_2)}")
+
+print(25 % 2)
+print(24 % 2)
+
+
+
