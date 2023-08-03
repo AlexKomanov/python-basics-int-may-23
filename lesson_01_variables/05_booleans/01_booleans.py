0eadfa4159426488b4482d3e404471738d8b685f
AlexKomanov <akomanov88@gmail.com>
2023-08-03 19:28:11 +0300

    Adding 01_booleans.py

---
+++ lesson_01_variables/05_booleans/01_booleans.py
@@ -0,0 +1,22 @@
+status = True
+my_status = False
+
+print(status)
+print(type(status))
+print(my_status)
+print(type(my_status))
+
+print(f"bool(1) -> {bool(1)}")
+print(f"bool(-1) -> {bool(-1)}")
+print(f"bool(0) -> {bool(0)}")
+print(f"bool(0.0011) -> {bool(0.0011)}")
+
+print(f"bool('A') -> {bool('A')}")
+print(f"bool('a') -> {bool('a')}")
+print(f"bool(' ') -> {bool(' ')}")
+print(f"bool('') -> {bool('')}")
+
+print(f"bool(None) -> {bool(None)}")
+
+
+
