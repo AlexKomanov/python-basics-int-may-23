bfdcdbc6688e8cb1734ec6933872dcc4841b3101
AlexKomanov <akomanov88@gmail.com>
2023-08-03 19:55:46 +0300

    adding 02_comparison_operators.py

---
+++ lesson_01_variables/05_booleans/02_comparison_operators.py
@@ -0,0 +1,33 @@
+# age = float(input("Please enter your age: "))
+# print(age >= 18)
+
+# number = int(input("Please enter a number: "))
+# print(number % 2 == 0)
+
+print(f"5 > 8 = {5 > 8}")
+print(f"5 < 8 = {5 < 8}")
+
+print(f"4 <= 4 = {4 <= 4}")
+print(f"3 <= 4 = {3 <= 4}")
+
+print(f"4 >= 4 = {4 >= 4}")
+print(f"5 >= 4 = {5 >= 4}")
+print(f"3 >= 4 = {3 >= 4}")
+
+print(f"4 == 4 = {4 == 4}")
+print(f"5 == 4 = {5 == 4}")
+
+print(f"4 != 4 = {4 == 4}")
+print(f"5 != 4 = {5 == 4}")
+
+# iIs number Odd or Even ???
+print(f"4 % 2 == 0 -> {4 % 2 == 0}")
+print(f"5 % 2 == 0 -> {5 % 2 == 0}")
+
+
+
+
+
+
+
+
