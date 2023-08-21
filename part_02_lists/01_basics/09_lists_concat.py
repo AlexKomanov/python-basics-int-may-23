f72f884358211638be2e6935be9b27a695505474
AlexKomanov <akomanov88@gmail.com>
2023-08-21 19:18:38 +0300

    Adding list example

--- part_02_lists/01_basics/09_lists_concat.py
+++ part_02_lists/01_basics/09_lists_concat.py
@@ -6,5 +6,10 @@ list_1 += ['Shaun', 'Yan', 'Omri']
 
 print(f"{list_1 = }")
 
-list_1 += 'Noam'
+list_1 += 'Noam Noam'
+print(f"{list_1 = }")
+
+# list_1 += 888 # TypeError: 'int' object is not iterable
+
+list_1 += [888]
 print(f"{list_1 = }")
