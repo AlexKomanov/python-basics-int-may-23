e4270419fc57ac3b344a25170aaaf61254abbc4f
AlexKomanov <akomanov88@gmail.com>
2023-09-04 19:43:54 +0300

    Adding conditions

---
+++ part_05_conditional_statements/03_if_elif_else_statement/03_several_conditions.py
@@ -0,0 +1,20 @@
+experience = int(input("Enter your experience: "))
+language = input("Enter your language: ")
+
+# if experience >= 3 and language == "Python":
+#     print("You're matching a Python job!")
+# elif experience >= 2 and language == "Java":
+#     print("You're matching a Java job!")
+# elif experience >= 5 and language == "JavaScript":
+#     print("You're matching a JavaScript job!")
+# else:
+#     print("You need to improve your skills!")
+
+
+if experience >= 2:
+    if language == "Java" or language == "JavaScript" or language == "Python":
+        print("You're matching a job!")
+    else:
+        print("Not required languages")
+else:
+    print("You need to improve your skills!")
