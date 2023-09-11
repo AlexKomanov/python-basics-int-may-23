e2135e8cd82b12fe4738c5af24d53c1dcc17297f
AlexKomanov <akomanov88@gmail.com>
2023-09-11 19:50:43 +0300

    Adding current state of project

--- part_06_loops/class_tasks/14_solution.py
+++ part_06_loops/class_tasks/14_solution.py
@@ -2,7 +2,7 @@ for i in range(1, 10):
     print(i, end=" ")
     for j in range(1, 10):
         # print("{:3d}".format(i * j), end=" ")  #  double-digit numbers
-        print(i * j, end="  ")
+        print(i * j, end="\t")
     print()
 
 # for i in range(1, 10):
