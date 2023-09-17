2d1e5d760c7b20617b5bf7fb50224ed6c351ce4c
AlexKomanov <akomanov88@gmail.com>
2023-09-17 16:31:16 +0300

    Adding current state of project

--- part_09_modules/part_01_direct_import/calculation_module.py
+++
@@ -1,5 +0,0 @@
-def calculate_sum(a: int, b: int):
-    return a + b
-
-def calculate_multiplication(a: int, b: int, c: int):
-    return a * b * c
--- part_09_modules/part_01_direct_import/import_reduce_example.py
+++
@@ -1,7 +0,0 @@
-from functools import reduce
-# from functools import *
-
-my_list = [5, 10, 15, 20, 25]
-
-total = reduce(lambda x, y: x + y, my_list)
-print(total)
