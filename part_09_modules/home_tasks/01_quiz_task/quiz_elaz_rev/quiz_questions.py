5e4194846ae9b0188079ca3a11518c0e28e6e2e9
elazrev <129072537+elazrev@users.noreply.github.com>
2023-09-28 20:58:03 +0300

    Create quiz_questions.py

---
+++ part_09_modules/home_tasks/01_quiz_task/quiz_elaz_rev/quiz_questions.py
@@ -0,0 +1,12 @@
+game_questions = {
+    "What is the capital of France?": "Paris",
+    "Which planet is known as the Red Planet?": "Mars",
+    "What is the largest mammal in the world?": "Blue whale",
+    "What is the chemical symbol for gold?": "Au",
+    "Who wrote the play 'Romeo and Juliet'?": "William Shakespeare",
+    "What is the largest organ in the human body?": "Skin",
+    "How many continents are there on Earth?": "7",
+    "Which gas do plants absorb from the atmosphere during photosynthesis?": "Carbon dioxide",
+    "Who painted the Mona Lisa?": "Leonardo da Vinci",
+    "What is the smallest prime number?": "2",
+}
