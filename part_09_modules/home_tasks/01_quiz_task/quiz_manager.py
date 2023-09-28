e2411f4fa5509895d481221a25523b7ceec0a06c
AlexKomanov <akomanov88@gmail.com>
2023-09-28 22:31:45 +0300

    Adding typing of the get_random_question() return

--- part_09_modules/home_tasks/01_quiz_task/quiz_manager.py
+++ part_09_modules/home_tasks/01_quiz_task/quiz_manager.py
@@ -1,10 +1,12 @@
 import random
+from typing import Tuple
+
 from quiz_questions import *
 
 user_score = 0  # Keeps track of the user's score.
 
 
-def get_random_question():  # Selects a random question from the question dictionary.
+def get_random_question() -> Tuple[str, str]:  # Selects a random question from the question dictionary.
     question, answer = random.choice(list(game_questions.items()))  # ('What is the capital of France?', 'Paris')
     # game_questions.pop(question)
     return question, answer
