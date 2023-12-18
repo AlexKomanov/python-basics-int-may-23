d259faa7b752b8525f3fbeeb77f3e87fa5399aa9
AlexKomanov <akomanov88@gmail.com>
2023-12-18 19:53:52 +0200

    adding solution 1

---
+++ part_12_oop/class_tasks/solution_01.py
@@ -0,0 +1,43 @@
+class Human:
+
+    def __init__(self, health: int):
+        self.health = health
+
+    def attack(self):
+        print("Human is attacking!")
+
+    def upgrade(self):
+        print(f"Upgrading to {self.health + 100}")
+
+
+class Warrior(Human):
+    def __init__(self, health: int, defense: float):
+        #    Human(250)
+        super().__init__(health)
+        self.defense = defense
+
+    def attack(self):
+        print("Warrior is attacking!")
+
+
+class Barbarian(Human):
+
+    def __init__(self, health: int, damage: int):
+        super().__init__(health)
+        self.damage = damage
+
+
+class Person(Human):
+    pass
+
+
+human = Human(250)
+warrior = Warrior(150, 25.9)
+barbarian = Barbarian(110, 300)
+person = Person(250)
+
+human.attack()
+warrior.attack()
+barbarian.attack()
+warrior.upgrade()
+barbarian.upgrade()
