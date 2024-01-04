adfe2ba1f014faa5b9740fa4f35e66bfd3ecae07
AlexKomanov <akomanov88@gmail.com>
2024-01-04 18:39:24 +0200

    Adding 03_different_types_handler.py

---
+++ part_16_telegram_bots/03_different_types_handler.py
@@ -0,0 +1,34 @@
+import asyncio
+import logging
+from aiogram import Bot
+from aiogram import Dispatcher
+from aiogram import types
+
+BOT_TOKEN = "6373447736:AAEv7uj3zgSXj9u4uQqDkp30FqS-baEiYC0"
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+
+@dp.message()
+async def answer_as_echo(message: types.Message):
+    if message.text:
+        await message.answer(text=message.text)
+    elif message.sticker:
+        await message.answer("Sticker was used...")
+        await message.reply_sticker(sticker=message.sticker.file_id)
+    else:
+        await message.reply("A new media type was used!!! ")
+
+
+@dp.message()
+async def reply_as_echo(message: types.Message):
+    await message.reply(text=message.text)
+
+
+async def main():
+    logging.basicConfig(level=logging.INFO)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
